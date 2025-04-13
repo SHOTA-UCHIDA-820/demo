e2c746b2519a81212b0e61f18eaccc7b96d98567
SHOTA <subaseball820@gmail.com>
2025-04-13 15:00:54 +0900

    04/13commit

---
+++ ConditionBranch.java
@@ -0,0 +1,36 @@
+public class ConditionBranch {
+
+    public void outputScore(int score) {
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+
+    public void outputMenu(int menuNo) {
+        switch (menuNo) {
+            case 10:
+                System.out.println("うどん");
+                break;
+            case 20:
+                System.out.println("そば");
+                break;
+            case 30:
+                System.out.println("パスタ");
+                break;
+            case 40:
+                System.out.println("ご飯");
+                break;
+            default:
+                System.out.println("その他");
+                break;
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        ConditionBranch cb = new ConditionBranch();
+
+        cb.outputScore(85);   
+        cb.outputMenu(30);
+    }
+}
